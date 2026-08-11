752b6154fe69691c485fb5de3d66edf39af8b074
THeInfinityPro <sasijagadish@gmail.com>
2026-08-11 18:28:17 +0530

    installation setup added

--- Scripts/docker.sh
+++ Scripts/docker.sh
@@ -27,12 +27,12 @@ check_supported_os() {
     fi
 
     case "$OS_VERSION" in
-        22.04|24.04)
+        22.04|24.04|26.04)
             success "Supported Ubuntu version detected: $OS_VERSION"
             ;;
         *)
             error "Unsupported Ubuntu version: $OS_VERSION"
-            error "Supported versions: Ubuntu 22.04 and 24.04"
+            error "Supported versions: Ubuntu 22.04 , 24.04 and 26.04"
             exit 1
             ;;
     esac
